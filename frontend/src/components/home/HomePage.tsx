90f4d7576d19d620c2b0987fccb6ac69fe982b22
ViniciusGilSousa <vinicius.sousa@vulcabras.com>
2024-10-16 22:40:02 -0300

    atualizando

---
+++ frontend/src/components/home/HomePage.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function HomePage() {
+    return (
+        <Link href="/" className="flex items-center gap-3 container">
+            <Image src="/logo-prefeitura.png" width={180} height={0} alt="logo-prefeitura" className='max-w-full h-auto my-2'  />
+        </Link>
+    )
+}
